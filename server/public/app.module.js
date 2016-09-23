8505b2541af879e4382d43ca4db6876e0cfae5ed
ilegra000035 <jonrmar@gmail.com>
2016-09-23 17:20:27 -0300

    removing folders from file-uploader

--- server/public/app.module.js
+++
@@ -1,1 +0,0 @@
-var app = angular.module('uploadApp', ['ngRoute']);
--- server/public/report/report.controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('uploadApp')
-		.controller('ReportController', ['$scope','$http', function( $scope, $http ) {
-			$scope.content = 'Output file not ready!';
-
-			$http.get('/report').then(function(data) {
-				$scope.content = data.data;
-			});
-
-		}]);
